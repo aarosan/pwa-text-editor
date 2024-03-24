9fb4282818d8132efda67ab9a3a424c942db8da1
aaron-ross-sanchez <147529595+aaron-ross-sanchez@users.noreply.github.com>
2024-03-24 18:46:11 -0500

    webpack.config

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -1,5 +1,6 @@
 const path = require('path');
 
+// When the server runs, the html file that was created from the webpack will be sent to the client
 module.exports = (app) =>
   app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '../client/dist/index.html'))
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serves static files in the entire client's dist folder
+// dist folder is created with webpack build
 app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
