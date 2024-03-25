e40ebfab6cc00297f012abe692895846c9394d67
aaron-ross-sanchez <147529595+aaron-ross-sanchez@users.noreply.github.com>
2024-03-25 13:56:54 -0500

    put and get request finished

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,30 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  try {
+    await store.put({ value: content });
+    console.log('Data updated and saved to the database');
+  } finally {
+    await tx.done;
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  try {
+    const result = await store.getAll();
+    console.log('result.value', result);
+    return result;
+  } finally {
+    await tx.done;
+  }
+};
 
 initdb();
